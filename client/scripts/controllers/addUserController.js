f7594367119993cd0cd3b94f897aeef3da25b393
Mbob023 <armcgrathmedia@gmail.com>
2016-05-02 14:50:13 -0500

    add user form clears after submit

--- client/scripts/controllers/addUserController.js
+++ client/scripts/controllers/addUserController.js
@@ -5,11 +5,21 @@ myApp.controller('AddUserController', ['$scope', 'UserService', function($scope,
 
     console.log('AddUserController Works!');
 
+    // Redeclares UserService
     var userService = UserService;
+
+    // Empty object to store new User data that the Admin inputs
     $scope.addUser = {};
 
+
+    // Function that runs the new User data through the newUser() function in UserService
     $scope.submit = function(data) {
         console.log('@mainController.js AddUserController data (AKA: addUser: ', data);
+
+        // Function to POST new User data to server
         userService.newUser(data);
+
+        // Clears addUser object
+        $scope.addUser = {};
     };
 }]);
