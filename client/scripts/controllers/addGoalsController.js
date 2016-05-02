22c06babd35c03746764c4aa15e1c6a0587e2a3c
Mbob023 <armcgrathmedia@gmail.com>
2016-05-02 14:41:04 -0500

    goals form clears after submit

--- client/scripts/controllers/addGoalsController.js
+++ client/scripts/controllers/addGoalsController.js
@@ -4,10 +4,16 @@ myApp.controller('AddGoalsController', ['$scope', 'GoalService', function($scope
 
   console.log('HI, @CONTROLLER - AddGoalsController Works!');
 
+  // Renames GoalService
   var goalService = GoalService;
 
+
+  // Empty object to store the goals entered by the User
   $scope.goals = {};
-  // $scope.months = ['september', 'october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august'];
+
+  // Object to populate the form with the months in order from Sept - Aug for the fiscal year
+  // $scope.monthsObj.name is for any labeling on the DOM
+  // $scope.monthsObj.key is for creating keys in $scope.goals object
   $scope.monthsObj = {
     months : [
       {
@@ -60,8 +66,15 @@ myApp.controller('AddGoalsController', ['$scope', 'GoalService', function($scope
       }
     ]
   };
+
+  // To save the User's input for the fiscal year. Will need to change to a selection menu
   $scope.fiscalyear = '';
 
+  // Need to update the Total Individual Goals for each month live!
+  // $scope.individualTotals = $scope.monthsObj.staff + $scope.monthsObj.board + $scope.monthsObj.committee + $scope.monthsObj.parent + $scope.monthsObj.alum + $scope.monthsObj.participant;
+
+
+  // Function to add the fiscal year to the $scope.goal object once the user is not focused on the fiscal year input
   $scope.addFyKey = function(year) {
     $scope.fiscalyear = year;
     return $scope.fiscalyear;
@@ -69,16 +82,20 @@ myApp.controller('AddGoalsController', ['$scope', 'GoalService', function($scope
 
   // console.log('~ @goalController after addFyKey - $scope.goals: ', $scope.goals);
 
+  // Function that process the data/goals inputed by the User and sends it through the newGoals() function in the GoalService
   $scope.saveGoals = function(data) {
-    var addGoals = {};
-    console.log('@goalController.js data (AKA: goals: ', data);
-    console.log('@goalController.js fiscalyear: ', $scope.fiscalyear);
 
-    // $scope.key = $scope.fiscalyear;
-    // console.log('## @goalController in saveGoals - $scope.key, data: ', $scope.key, data);
-    // addGoals[$scope.key] = data;
+    // var addGoals = {};
+
+    // Console.logs - to verify information and confirm the data and $scope.fiscalyear
+    console.log('@goalController.js fiscalyear: ', $scope.fiscalyear);
     console.log('! @goalController in saveGoals - data: ', data);
+
+    // Run function newGoals() from GoalService that POSTs that data to the server
     goalService.newGoals(data);
+
+    // Clears the goals object
+    $scope.goals = {};
   };
 
 }]);
